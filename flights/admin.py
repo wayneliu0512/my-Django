64c91e28310c5c6f29f62ccf99994b94dcab7fc9
wayne <wayneliu0512@gmail.com>
2018-11-07 21:30:34 +0800

    finish my

--- flights/admin.py
+++ flights/admin.py
@@ -3,6 +3,17 @@ from django.contrib import admin
 from .models import Flight, Airport, Passenger
 
 # Register your models here.
+
+class PassengerInline(admin.StackedInline):
+    model = Passenger.flights.through
+    extra = 1
+
+class FlightAdmin(admin.ModelAdmin):
+    inlines = [PassengerInline]
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights",)
+
 admin.site.register(Airport)
-admin.site.register(Flight)
-admin.site.register(Passenger)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
